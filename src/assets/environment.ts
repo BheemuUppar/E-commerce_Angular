30eef927ae7b5da94e9d308f888568c7117bb5aa
bheemuk123 <bheemuk123@gmail.com>
2024-03-17 15:34:25 +0530

    added live apis

--- src/assets/environment.ts
+++ src/assets/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000',
-  register: 'http://localhost:3000/auth/register',
-  login: 'http://localhost:3000/auth/login',
-  createOrder:'http://localhost:3000/payment/createOrder',
-  tokenVerify:'https://localhost:3000/auth/verifyToken',
-  paymentVerify:'http://localhost:3000/payment/verify',
-  getOrders:"http://localhost:3000/user/orders",
-  orderCash:'http://localhost:3000/user/order-cash',
+  baseUrl: 'https://e-commerce-nodejs-ik6r.onrender.com',
+  register: 'https://e-commerce-nodejs-ik6r.onrender.com/auth/register',
+  login: 'https://e-commerce-nodejs-ik6r.onrender.com/auth/login',
+  createOrder:'https://e-commerce-nodejs-ik6r.onrender.com/payment/createOrder',
+  tokenVerify:'https://e-commerce-nodejs-ik6r.onrender.com/auth/verifyToken',
+  paymentVerify:'https://e-commerce-nodejs-ik6r.onrender.com/payment/verify',
+  getOrders:"https://e-commerce-nodejs-ik6r.onrender.com/user/orders",
+  orderCash:'https://e-commerce-nodejs-ik6r.onrender.com/user/order-cash',
 
   razorpayKey:'rzp_test_GUCLqEesz1MaZA',
 };
